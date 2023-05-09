abea70b9cbe7ec137c93a40101d2fb37f1772acc
anmutetech <124485136+anmutetech@users.noreply.github.com>
2023-05-09 16:25:39 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,10 @@ output "web_instance_ip" {
 }
 
 output "instance_id" {
-    value = aws_instance.web.public_ip
+    value = aws_instance.web.id
+}
+
+output "private_key" {
+    value = tls_private_key.key_type.private_key_pem
+    sensitive = true
 }
